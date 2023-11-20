d1de72038efbf32326478899a7945934b5bec721
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-20 15:23:05 +0100

    feat: array con i membri e le info

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict'
+
+// Arrey dei membri
+const teamMembers = [];
+
+// Oggetti per ogni membro del team che poi vengono agginti all'array
+
+//Membro 1
+let member1 = {
+    nome: "Wayne Bernet",
+    ruolo: "Founder & CEO",
+    foto: "wayne-barnett-founder-ceo.jpg"
+}
+
+teamMembers.push(member1);
+
+//Membro 2
+let member2 = {
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    foto: "angela-caroll-chief-editor.jpg"
+}
+
+teamMembers.push(member2);
+
+//Membro 3
+let member3 = {
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+    foto: "walter-gordon-office-manager.jpg"
+}
+
+teamMembers.push(member3);
+
+//Membro 4
+let member4 = {
+    nome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+    foto: "angela-lopez-social-media-manager.jpg"
+}
+
+teamMembers.push(member4);
+
+//Membro 5
+let member5 = {
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+    foto: "scott-estrada-developer.jpg"
+}
+
+teamMembers.push(member5);
+
+//Membro 6
+let member6 = {
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    foto: "barbara-ramos-graphic-designer.jpg"
+}
+
+teamMembers.push(member6);
+
+console.log (teamMembers)
